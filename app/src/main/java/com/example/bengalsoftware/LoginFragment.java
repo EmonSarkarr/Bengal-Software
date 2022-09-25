153980be0f2c0c9f91c53fadeae7dbb20e06468e
EMON <emonsarkar1111@gmail.com>
2022-09-25 16:26:40 +0600

    Login ViewModel and Login Fragment

--- app/src/main/java/com/example/bengalsoftware/LoginFragment.java
+++ app/src/main/java/com/example/bengalsoftware/LoginFragment.java
@@ -3,26 +3,60 @@ package com.example.bengalsoftware;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bengalsoftware.databinding.FragmentLoginBinding;
+import com.example.bengalsoftware.viewmodels.LoginViewModel;
 
 public class LoginFragment extends Fragment {
 
+    private LoginViewModel loginViewModel;
     private FragmentLoginBinding binding;
+    private boolean isLogin;
 
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
+    public LoginFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(inflater,container,false);
+        loginViewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+        binding.loginBtn.setOnClickListener( view -> {
+            isLogin = true;
+            authenticate();
+        });
+        binding.registerBtn.setOnClickListener(view -> {
+            isLogin = false;
+            authenticate();
+        });
+
+
+        loginViewModel.getStateLiveData()
+                .observe(getViewLifecycleOwner(), authState -> {
+                    if (authState == LoginViewModel.AuthState.AUTHENTICATED) {
+                        Navigation.findNavController(container)
+                                .navigate(R.id.login_to_home_Action);
+                    }
+                });
+
+        loginViewModel.getErrMsgLiveData().observe(getViewLifecycleOwner(), errMsg -> {
+                    binding.errorMsgTV.setText(errMsg);
+                });
         return binding.getRoot();
+    }
+    private void authenticate() {
+        final String email = binding.emailInputET.getText().toString();
+        final String password = binding.passwordInputET.getText().toString();
+
+        if (isLogin) {  //we have to pass the context email and password
+            loginViewModel.login(email, password);
+        }else {
+            loginViewModel.register(email, password);
+        }
     }
 }
